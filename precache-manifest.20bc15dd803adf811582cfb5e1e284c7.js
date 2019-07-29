376061f119604e96a18d27214ddc1ca7baf2b7f5
rui.liang <rui.liang@kaiostech.com>
2019-07-29 14:36:21 +0800

    first commit

---
+++ precache-manifest.20bc15dd803adf811582cfb5e1e284c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189cb46950d0f6caa54351788144c108",
+    "url": "/react_pwa/index.html"
+  },
+  {
+    "revision": "02e40c16c4b8a32435b4",
+    "url": "/react_pwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_pwa/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "02e40c16c4b8a32435b4",
+    "url": "/react_pwa/static/js/main.8b94d2c6.chunk.js"
+  },
+  {
+    "revision": "01ce61b8b892c276c61e",
+    "url": "/react_pwa/static/js/runtime~main.d4194f84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
